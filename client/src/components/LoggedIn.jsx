252d0976e52e94eaef140f4eec9f970e114eac40
mabatemarco <mabatemarco928@gmail.com>
2019-11-08 16:48:13 -0500

    react skeleton

---
+++ client/src/components/LoggedIn.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Profile from './Profile'
+import ProjectList from './ProjectList'
+import Project from './Project'
+import Home from './Home'
+
+class LoggedIn extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <p>hi</p>
+    )
+  }
+}
+
+export default LoggedIn
