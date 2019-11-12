44ea890571ed4dc8b51991fb70ad38d87675224c
mabatemarco <mabatemarco928@gmail.com>
2019-11-12 10:26:27 -0500

    deleted login on welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,20 +3,19 @@ import image from '../images/logowte.png'
 
 
 export default function Welcome(props) {
-  const {loginData} = props
+  const { loginData } = props
   return (
     <div className='welcome-pg'>
-<button id='btn-log'>Log in</button>
-    {/* <img id='logo-collab' src={image} width='110px' height='110px'   alt='sourc logo' /> */}
+
       <h1 className='title'>Collaborate with Software Developers and UX/I designers on projects.</h1>
       <div className='welcome-para'>
-      <h2>Join teams.</h2>
-      <h2>Build your network.</h2>
+        <h2>Join teams.</h2>
+        <h2>Build your network.</h2>
         <h2>Socialize.</h2>
         <h2>Find a S( )urc!</h2>
       </div>
       <button onClick='' id='btn-sign'>Sign Up</button>
-      
+
       {/* <form onSubmit={handleSubmit} >
         <label htmlFor='username'>Username</label>
         <input name='username' type='text' value={loginData} onChange={handleChange} />
