861816eb1bfc8dc0ecb79877a2e19a0ec43cd5ac
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-11-13 12:00:58 -0500

    weldone2

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Profile (){
   return (
-    <></>
+    <div className='pro-pg'>
+    
+    <h1>Profile</h1>
+    
+    
+    </div>
   )
 }
