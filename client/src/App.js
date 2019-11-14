187a2f207c92cedbe51b11d93a6cec87b0c7e849
Cordero <ChristopherCordero29@gmail.com>
2019-11-14 17:55:56 -0500

    about page finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Link, Route, withRouter } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Footer from './components/Footer'
 import Register from './components/Register'
 import LoggedIn from './components/LoggedIn'
 import Welcome from './components/Welcome'
 import Login from './components/Login';
-import About from './components/About'
 import { verifyUser, loginUser, registerUser } from './services/api-helper'
 
 
